a6802fbbfc76b1f8506e7d613b78c4bbf4e2c29a
Fernán González Domingo <fernangonzalez@gmail.com>
2018-01-18 22:14:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
-  const array = [element, ...array]
+
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
