9ee6e5a703ec9d42cd2c73752d94f0bca902f12f
Fernán González Domingo <fernangonzalez@gmail.com>
2018-01-18 22:22:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,6 +20,7 @@ function destructivelyAddElementToEndOfArray(array, element){
   return array;
 }
 
-function accessElementInArray(){
+function accessElementInArray(array, index) {
+  return array[index];
   
 }
